87a004f043d9e4ed36862fe18c97aa51cb77c335
Raphael Zenhäusern <raphael@zenhausern.net>
2015-10-28 22:49:10 +0100

    Zwischencommit für erste Präsentation

--- ProjectEnton/Views/Shell.xaml.cs
+++ ProjectEnton/Views/Shell.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
-namespace ProjectEnton
+namespace ProjectEnton.Views
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Shell : Page
     {
-        public MainPage()
+        public Shell()
         {
             this.InitializeComponent();
-        }
-
-        private void TextBlock_ContextMenuOpening(object sender, ContextMenuEventArgs e)
-        {
-
         }
     }
 }
