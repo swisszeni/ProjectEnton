da93f78169ffd7e1cd07dfeb814e7f4abca9777d
Florian Schnyder <flo88@gmx.ch>
2016-01-13 17:41:37 +0100

    Improved layout, add code for tests

--- ProjectEnton/Views/TakingPlanPage.xaml.cs
+++ ProjectEnton/Views/TakingPlanPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectEnton.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,59 @@ namespace ProjectEnton.Views
         public TakingPlanPage()
         {
             this.InitializeComponent();
+
+            /// Add todays date (with the days name) to the view
+            TodaysDate.Text = DateTime.Now.ToString("dddd, dd.MM.yyy");
+
+            ///For tests: generates a TakingPlan object
+            Drug d = new Drug(2, "Dafalgan", "Ethanol", 2, "Pulver", null);
+            TakingPlan dafalgan = new TakingPlan(d, new DateTime(2016,1,1), new DateTime(2016, 12, 12), new DayTime(), null);
+
+            // Add the drug to the page (morning, noon and evening)
+            morningListView.Items.Add(dafalgan.drug.ToString());
+            noonListView.Items.Add(dafalgan.drug.ToString());
+            eveningListView.Items.Add(dafalgan.drug.ToString());
+
+            // Check if a Listview ist empty. If a view is empty, it won't be displayed
+            if(morningListView.Items.Count == 0) { MorningPanel.Visibility = Visibility.Collapsed; }
+            if (noonListView.Items.Count == 0) { NoonPanel.Visibility = Visibility.Collapsed; }
+            if (eveningListView.Items.Count == 0) { EveningPanel.Visibility = Visibility.Collapsed; }
+            if (nightListView.Items.Count == 0) { NightPanel.Visibility = Visibility.Collapsed; }
+
+            //test with enum, but could not get the digits out of it.... :(
+            /*
+            int morningAndNight = 1001;
+            dafalgan.takingDayTime. = (DayTime)morningAndNight;
+            
+            //Check if the transformation 
+            int test = (int)dafalgan.takingDayTime;
+           
+            byte[] result = BitConverter.GetBytes(test);
+
+            morningListView.Items.Add(Enum.GetValues(typeof(DayTime)));
+            foreach(var a in result)
+            {
+                morningListView.Items.Add(a);
+            }
+            if (result[0] == 1)
+            {
+                morningListView.Items.Add(dafalgan.drug.ToString());
+            }
+
+            if (result[1] == 1)
+            {
+                noonListView.Items.Add(dafalgan.drug.ToString());
+            }
+
+            if (result[2] == 1)
+            {
+                eveningListView.Items.Add(dafalgan.drug.ToString());
+            }
+
+            if (result[3] == 1)
+            {
+                nightListView.Items.Add(dafalgan.drug.ToString());
+            }*/
         }
     }
 }
