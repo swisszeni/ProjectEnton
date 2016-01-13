df96b1b5e9b351b9f528bc61dd8ddd9264bd5b4c
Raphael Zenhäusern <raphael@zenhausern.net>
2016-01-13 04:00:29 +0100

    Improved overall layout.

--- ProjectEnton/Views/SearchDrug.xaml.cs
+++ ProjectEnton/Views/SearchDrug.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
-namespace ProjectEnton
+namespace ProjectEnton.Views
 {
     /// <summary>
     /// This page is responsible for the drug search. The user can either enter a text or scan a barcode to get the needed drug.
